6ee6936dbdb01925f8eaf4a127f2dd7fa0e3b06e
vansh123456 <vanshbhatnagar14dev@gmail.com>
2025-12-15 19:05:14 +0530

    [DNM]more comments will squash later

--- backend/src/service/boardingService.js
+++ backend/src/service/boardingService.js
@@ -1,24 +1,24 @@
 import { extractSeatNumber } from "../utils/seatUtils.js";
 
 export function generateBoardingSequence(bookings) {
-  const enrichedBookings = bookings.map(b => {
-    const maxSeatDistance = Math.max(
-      ...b.seats.map(seat => extractSeatNumber(seat))
+  const enrichedBookings = bookings.map(b => {  //loop through each booking
+    const maxSeatDistance = Math.max( //find farthest seat first
+      ...b.seats.map(seat => extractSeatNumber(seat)) //... hai spread operator that expands an array into individual elements
     );
 
     return {
       bookingId: b.bookingId,
-      maxSeatDistance
+      maxSeatDistance //reassign usi main kar rahe to no declaration
     };
   });
 
-  enrichedBookings.sort((a, b) => { //comparator se kar rahe
-    if (b.maxSeatDistance !== a.maxSeatDistance) {
+  enrichedBookings.sort((a, b) => { //comparator se kar rahe  ki kon pehle board karega
+    if (b.maxSeatDistance !== a.maxSeatDistance) { //same booking time pe pehle wala booking crow
       return b.maxSeatDistance - a.maxSeatDistance; //max distance se minus kar rahe to get distance
     } //result is positive
 
  //a moves after b
-    return a.bookingId - b.bookingId;
+    return a.bookingId - b.bookingId;  //smaller booking wala board pehele
   });
 
   return enrichedBookings.map((b, index) => ({
